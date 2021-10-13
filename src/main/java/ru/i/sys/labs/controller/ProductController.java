3d3cf06b4b3dca6928f4975404b62f4a21f19a08
SOz03 <game.no4ta@gmail.com>
2021-10-13 19:26:10 +0400

    Added serviceController and ProductControllerService. Change ProductController.

--- src/main/java/ru/i/sys/labs/controller/ProductController.java
+++ src/main/java/ru/i/sys/labs/controller/ProductController.java
@@ -1,65 +1,50 @@
 package ru.i.sys.labs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.i.sys.labs.entity.Product;
 import ru.i.sys.labs.exception.ResourceNotFoundException;
-import ru.i.sys.labs.serviceDAO.ProductRepositoryDAO;
+import ru.i.sys.labs.serviceController.ProductControllerService;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/shop")
 public class ProductController {
 
-    private final ProductRepositoryDAO productRepositoryDAO;
+    private final ProductControllerService productControllerService;
 
     @Autowired
-    public ProductController(ProductRepositoryDAO productRepositoryDAO) {
-        this.productRepositoryDAO = productRepositoryDAO;
+    public ProductController(ProductControllerService productControllerService) {
+        this.productControllerService = productControllerService;
     }
 
     @GetMapping("/products")
     public List<Product> getAllProducts() {
-        return productRepositoryDAO.findAll();
+        return productControllerService.getAllProducts();
     }
 
     @PostMapping("/products")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        productRepositoryDAO.save(product);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return productControllerService.createProduct(product);
     }
 
     @GetMapping("/products/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable(value = "id") UUID id) throws ResourceNotFoundException {
-        Product product = productRepositoryDAO
-                .findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Не найден" + id));
-        return ResponseEntity.ok().body(product);
+        return productControllerService.getProductById(id);
     }
 
     @PutMapping("/products/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable(value = "id") UUID id,
                                                  @RequestBody Product productUpdate) throws ResourceNotFoundException {
-        Product product = productRepositoryDAO
-                .findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Не найден" + id));
-        product.setName(productUpdate.getName());
-        product.setPrice(productUpdate.getPrice());
-        product.setProductionDate(productUpdate.getProductionDate());
-        product.setDescription(productUpdate.getDescription());
-        productRepositoryDAO.save(product);
-        return ResponseEntity.ok().body(product);
+        return productControllerService.updateProduct(id, productUpdate);
     }
 
     @DeleteMapping("/products/{id}")
-    public ResponseEntity<Product>  deleteProduct(@PathVariable(value = "id") UUID id) throws ResourceNotFoundException{
-        productRepositoryDAO.findById(id).orElseThrow(() -> new ResourceNotFoundException("Не найден" + id));
-        productRepositoryDAO.deleteById(id);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Product> deleteProduct(@PathVariable(value = "id") UUID id) throws ResourceNotFoundException {
+        return productControllerService.deleteProduct(id);
     }
 
 }
---
+++ src/main/java/ru/i/sys/labs/serviceController/ProductControllerService.java
@@ -0,0 +1,57 @@
+package ru.i.sys.labs.serviceController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import ru.i.sys.labs.entity.Product;
+import ru.i.sys.labs.exception.ResourceNotFoundException;
+import ru.i.sys.labs.serviceDAO.ProductRepositoryDAO;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProductControllerService {
+    private final ProductRepositoryDAO productRepositoryDAO;
+
+    @Autowired
+    public ProductControllerService(ProductRepositoryDAO productRepositoryDAO) {
+        this.productRepositoryDAO = productRepositoryDAO;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepositoryDAO.findAll();
+    }
+
+    public ResponseEntity<Product> createProduct(Product product) {
+        productRepositoryDAO.save(product);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Product> getProductById(UUID id) throws ResourceNotFoundException {
+        Product product = productRepositoryDAO
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Не найден" + id));
+        return ResponseEntity.ok().body(product);
+    }
+
+    public ResponseEntity<Product> updateProduct(UUID id, Product productUpdate) throws ResourceNotFoundException {
+        Product product = productRepositoryDAO
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Не найден" + id));
+        product.setName(productUpdate.getName());
+        product.setPrice(productUpdate.getPrice());
+        product.setProductionDate(productUpdate.getProductionDate());
+        product.setDescription(productUpdate.getDescription());
+        productRepositoryDAO.save(product);
+        return ResponseEntity.ok().body(product);
+    }
+
+    public ResponseEntity<Product> deleteProduct(UUID id) throws ResourceNotFoundException {
+        productRepositoryDAO.findById(id).orElseThrow(() -> new ResourceNotFoundException("Не найден" + id));
+        productRepositoryDAO.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
