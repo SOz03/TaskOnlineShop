420a46b1a230e3de3622d04e7c90bb44a891c035
SOz03 <game.no4ta@gmail.com>
2021-11-17 01:22:24 +0400

    Update logback for out file.

--- src/main/java/ru/i/sys/labs/Application.java
+++ src/main/java/ru/i/sys/labs/Application.java
@@ -2,6 +2,7 @@ package ru.i.sys.labs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
