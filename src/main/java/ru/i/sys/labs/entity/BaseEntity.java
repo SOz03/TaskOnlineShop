753c10212cd5712c38f3a51ff5bc9e66bfd6cd62
SOz03 <game.no4ta@gmail.com>
2021-11-17 14:58:52 +0400

    Added abstract class for Entity. Changed service and controller for BasketProduct. Changed class names.

---
+++ src/main/java/ru/i/sys/labs/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package ru.i.sys.labs.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.UUID;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    protected UUID id;
+
+    public BaseEntity() {
+        this.id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
