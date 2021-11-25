4b6c94a6b24690b06004d0f40444d8773040460c
s.ozerov <game.no4ta@gmail.com>
2021-11-25 13:47:52 +0400

    Completely redesigned notification system. v.2 UPDATE.

---
+++ src/main/java/ru/i/sys/labs/messages/SocialNotification.java
@@ -0,0 +1,22 @@
+package ru.i.sys.labs.messages;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.i.sys.labs.messages.property.GeneralProperty;
+
+@Component
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "spring.application.notification.enabled", havingValue = "true", matchIfMissing = true)
+public class SocialNotification implements NotificationSender {
+
+    private final NotificationService notificationService;
+    private final GeneralProperty property;
+
+    @Override
+    @Scheduled(cron = "${spring.application.notification.social.time}")
+    public void sendMessage() {
+        notificationService.handler(property.getSocialPrefix(), "social");
+    }
+}
